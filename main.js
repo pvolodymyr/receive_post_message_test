69788998aac1a30f44c870c0abe336f131604858
vpohorilyak <volodymir.pohorilyak@ext.etecture.de>
2024-02-20 18:08:05 +0100

    test

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
 const iframe = document.getElementById('iframe');
-
-iframe.addEventListener('load', function () {
-	console.log('🚀 ~ iframe loaded');
-
-	iframe.contentWindow.postMessage({
-		message: 'token',
-		token: 'some token',
-	});
-});
