2bc30d00cf35cbfd46b381540fcb480ec69bec37
vpohorilyak <volodymir.pohorilyak@ext.etecture.de>
2024-02-27 19:07:17 +0100

    test

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const iframe = document.getElementById('iframe');
 
 iframe.addEventListener('load', () => {
-	iframe.contentWindow.postMessage({
+	// iframe.contentWindow.postMessage({
+	// 	accessToken:
+	// 		'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJGaXhkIiwicm9sZSI6IlBhcnRuZXIiLCJ1bmlxdWVfbmFtZSI6IkZpeGQiLCJwYXJ0bmVyIjoyLCJuYmYiOjE3MDc5MTQ2NjksImV4cCI6MjAyMzI3NDY2OSwiaWF0IjoxNzA3OTE0NjY5LCJpc3MiOiJJc3N1ZXIiLCJhdWQiOiJBdWRpZW5jZSJ9.tD8Bl00iShNG-GEH2fbuHFEDXSheNwfB_IpNLWqPUfmPLnFLLZUfYfiJLmAm4WdZu9yzqlQ3YtXAla4vX9borQ',
+	// });
+
+	window.postMessage({
 		accessToken:
 			'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJGaXhkIiwicm9sZSI6IlBhcnRuZXIiLCJ1bmlxdWVfbmFtZSI6IkZpeGQiLCJwYXJ0bmVyIjoyLCJuYmYiOjE3MDc5MTQ2NjksImV4cCI6MjAyMzI3NDY2OSwiaWF0IjoxNzA3OTE0NjY5LCJpc3MiOiJJc3N1ZXIiLCJhdWQiOiJBdWRpZW5jZSJ9.tD8Bl00iShNG-GEH2fbuHFEDXSheNwfB_IpNLWqPUfmPLnFLLZUfYfiJLmAm4WdZu9yzqlQ3YtXAla4vX9borQ',
 	});
