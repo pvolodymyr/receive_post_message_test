7bb84bdf0d055898446442a114ee93d4dd58fa80
vpohorilyak <volodymir.pohorilyak@ext.etecture.de>
2025-04-18 16:32:06 +0200

    update initialization

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const config = {
-	accessToken:
-		'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJDb1BhcnRzIiwicm9sZSI6IlBhcnRuZXIiLCJ1bmlxdWVfbmFtZSI6IkNvUGFydHMiLCJwYXJ0bmVyIjoiQ29QYXJ0cyIsIm5iZiI6MTc0NDYyNzYwNywiZXhwIjoyMDU5OTg3NjA3LCJpYXQiOjE3NDQ2Mjc2MDcsImlzcyI6Iklzc3VlciIsImF1ZCI6IkF1ZGllbmNlIn0.oul_Gy0iWPH87vNNr3wgjAL7cbjqB7FTO8H4OglEYwa2hT4l-ap9Zq5Ti2YbK9MjZSWoefJUhXG3sFePvOexLg',
+	targetElementId: 'paymentIframeTargetElement',
 	userDetails: {
 		firstName: 'AQA Partner Order',
 		lastName: 'AQA Partner Order',
 		email: 'test@chaiz.com',
 		address: '200 W 54th St, New York, NY 10019, USA',
-		addressApartment: '1',
+		addressApartment: undefined,
 		city: 'New York',
 		state: 'NY',
 		zip: '10019',
 		phoneNumber: '1111111111',
 	},
 	searchResultPlanIds: [1542541],
+	overrides: {
+		purchaseButtonText: 'Buy',
+		stylesLink: 'https://payment-iframe.glitch.me/style.css',
+	},
 };
 
 if (window.Chaiz) {
-	const iframe = window.Chaiz.createPaymentIframe(config);
-	iframe.mount('paymentIframeTargetElement');
+	const iframe = window.Chaiz.createPaymentIframe(
+		'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJDb1BhcnRzIiwicm9sZSI6IlBhcnRuZXIiLCJ1bmlxdWVfbmFtZSI6IkNvUGFydHMiLCJwYXJ0bmVyIjoiQ29QYXJ0cyIsIm5iZiI6MTc0NDYyNzYwNywiZXhwIjoyMDU5OTg3NjA3LCJpYXQiOjE3NDQ2Mjc2MDcsImlzcyI6Iklzc3VlciIsImF1ZCI6IkF1ZGllbmNlIn0.oul_Gy0iWPH87vNNr3wgjAL7cbjqB7FTO8H4OglEYwa2hT4l-ap9Zq5Ti2YbK9MjZSWoefJUhXG3sFePvOexLg'
+	);
+	iframe.mount(config);
 }
